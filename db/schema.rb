bea09aca05b23f64d335e8e3cc93f8e741e70f3c
andras <andrasm96@gmail.com>
2021-09-09 09:59:22 -0700

    Add Devise user authentication

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_08_230710) do
+ActiveRecord::Schema.define(version: 2021_09_09_163013) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -128,6 +128,18 @@ ActiveRecord::Schema.define(version: 2021_09_08_230710) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["invoice_id"], name: "index_tickets_on_invoice_id"
     t.index ["technician_id"], name: "index_tickets_on_technician_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "complications", "complication_types"
