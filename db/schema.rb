e43b865bd4cb83f03acb2fff86b714fa7625cdbb
Brayden Brown <braydenbrown@gmail.com>
2021-09-29 16:42:24 -0700

    Add Mac System to gem-lock

--- db/schema.rb
+++ db/schema.rb
@@ -110,6 +110,7 @@ ActiveRecord::Schema.define(version: 2021_09_29_183104) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.text "notes"
+    t.string "int"
     t.integer "number"
     t.index ["customer_id"], name: "index_invoices_on_customer_id"
   end
