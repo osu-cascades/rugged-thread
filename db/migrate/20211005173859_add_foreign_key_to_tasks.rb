4cf66beee06df7e8779a2b225c6c8776fa29afa8
Yong Bakos <ybakos@humanoriented.com>
2021-10-28 12:01:12 -0700

    Migrations: Remove 20211005173859.
    This FK addition is no longer valid, as the original migration
    that created the tasks table was deleted.

--- db/migrate/20211005173859_add_foreign_key_to_tasks.rb
+++
@@ -1,7 +0,0 @@
-class AddForeignKeyToTasks < ActiveRecord::Migration[6.1]
-  def change
-    add_index :repairs, :number, unique: true
-
-    add_foreign_key :tasks, :repairs, column: :repair_number, primary_key: :number
-  end
-end
