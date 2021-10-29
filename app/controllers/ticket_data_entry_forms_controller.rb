0cadedc36d9a678678dacf13d8a1330cf6a5aaaa
Yong Bakos <ybakos@humanoriented.com>
2021-10-28 17:12:47 -0700

    TicketDataEntryFormController: Correct parameter cleansing.

--- app/controllers/ticket_data_entry_forms_controller.rb
+++ app/controllers/ticket_data_entry_forms_controller.rb
@@ -18,7 +18,6 @@ class TicketDataEntryFormsController < ApplicationController
 
     def create
       @ticket_data_entry_form = TicketDataEntryForm.new(ticket_data_entry_form_params)
-  
       if @ticket_data_entry_form.save
         redirect_to root_url, notice: 'Ticket Entry was submitted successfully.'
       else
@@ -53,6 +52,12 @@ class TicketDataEntryFormsController < ApplicationController
     end
   
     def ticket_data_entry_form_params
-      params.require(:ticket_data_entry_form).permit(:customer_name, :phone_number, :invoice_number, :estimate_number, :intake_date, :request_date, :order_type, :discount, :item_number, :item_type, :labor_charge, :material_charge, repair_attributes: [:id, :repair_description, :repair_quote, :repair_charge, :_destroy, task_attributes: [:id, :task_type, :technician_name, :task_time, :task_date, :_destroy]])
+      params.require(:ticket_data_entry_form).permit(
+        :customer_name, :phone_number, :invoice_number, :estimate_number,
+        :intake_date, :request_date, :order_type, :discount, :item_number,
+        :item_type, :labor_charge, :material_charge,
+        repairs: [:id, :description, :quote, :charge, :_destroy,
+          tasks: [:id, :task_type_name, :technician_name, :time, :date, :_destroy]])
     end
-  end
+
+end
