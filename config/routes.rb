2cefba59bf751699703951851713343018f0219f
Brayden Brown <braydenbrown@gmail.com>
2022-01-10 11:49:20 -0800

    Implement CRUD for fee list #11

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :fees
   resources :item_statuses
   resources :customer_types
   resources :tasks
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_10_194150) do
+ActiveRecord::Schema.define(version: 2022_01_10_194722) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -87,6 +87,13 @@ ActiveRecord::Schema.define(version: 2022_01_10_194150) do
     t.string "city"
     t.string "state"
     t.string "zip_code"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "fees", force: :cascade do |t|
+    t.text "description"
+    t.integer "price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
