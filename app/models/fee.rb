b7475984cff0a316a7fc0f77de9dabbffb12cc2a
Brayden Brown <braydenbrown@gmail.com>
2022-01-17 14:16:05 -0800

    Add system testing for Fees and validation for Fee entry #24

--- app/models/fee.rb
+++ app/models/fee.rb
@@ -1,2 +1,5 @@
 class Fee < ApplicationRecord
+  validates :description, presence: true
+  validates :description, uniqueness: true
+  validates :price , numericality: { only_integer: true }, allow_nil: true
 end
