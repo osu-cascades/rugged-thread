0db142ea6aaca0c08865f5d5817847048bf9fc77
andras <andrasm96@gmail.com>
2022-01-18 11:53:46 -0800

    Add Controller for Work Orders, format work order page per mockup

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
   resources :invoices
   resources :technicians
   resources :customers
+  resources :work_orders, only: [:edit]
   get "/pages/:page" => "pages#show"
 
 
