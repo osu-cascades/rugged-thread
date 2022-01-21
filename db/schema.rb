083b84f92f5da97e47999834d08f2d5896935e66
Brayden Brown <braydenbrown@gmail.com>
2022-01-21 10:40:01 -0800

    Add dollar and percentage ammount to discount list #25

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_20_000133) do
+ActiveRecord::Schema.define(version: 2022_01_21_183335) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -101,9 +101,10 @@ ActiveRecord::Schema.define(version: 2022_01_20_000133) do
 
   create_table "discounts", force: :cascade do |t|
     t.text "description"
-    t.integer "amount"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "percentage_amount"
+    t.integer "dollar_amount"
   end
 
   create_table "fees", force: :cascade do |t|
