0a331cf07bcee0e4426f7ff501c7db65ba26c7c9
Brayden Brown <braydenbrown@gmail.com>
2022-01-21 16:20:44 -0800

    Write unit tests for customer types #22

--- test/models/customer_type_test.rb
+++ test/models/customer_type_test.rb
@@ -1,7 +1,23 @@
 require "test_helper"
 
 class CustomerTypeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'Customer Type has a name' do
+    assert_respond_to(CustomerType.new, :name)
+  end
+
+
+  test 'Customer Type without a name is invalid' do
+    customer_type = customer_types(:one)
+    assert customer_type.valid?
+    customer_type.name = nil
+    refute customer_type.valid?
+  end
+
+  test 'Customer Type with a non-unique name is invalid' do
+    existing_customer_type_name = customer_types(:one).name
+    customer_type = customer_types(:two)
+    assert customer_type.valid?
+    customer_type.name = existing_customer_type_name
+    refute customer_type.valid?
+  end
 end
