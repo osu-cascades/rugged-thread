bb25818387e24590c60ff07690d8ecb6d0371e0a
Yong Bakos <ybakos@humanoriented.com>
2022-01-22 12:10:25 -0800

    ApplicationController: Adjust sign out path and clean up.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,16 @@
 class ApplicationController < ActionController::Base
-    before_action :authenticate_user!
+  include Pundit
 
-    include Pundit
+  before_action :authenticate_user!, unless: :devise_controller?
 
-    private 
+  private
     
     def restrict_unless_admin
-        redirect_to(root_url, alert: 'Access denied.') unless current_user.admin?
+      redirect_to(root_url, alert: 'Access denied.') unless current_user.admin?
     end
+
+    def after_sign_out_path_for(resource)
+      new_user_session_path
+    end
+
 end
