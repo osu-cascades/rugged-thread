801585d07504a02adbdaab1df0667ff1ba346910
Yong Bakos <ybakos@humanoriented.com>
2022-01-22 02:18:02 -0800

    User: Add Devise trackable fields.
    References #20, #27.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :trackable,
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true, allow_blank: false
