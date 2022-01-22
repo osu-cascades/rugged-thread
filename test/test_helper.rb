a7dbef983ed6d9516b3b25338da07464177a3d4f
Yong Bakos <ybakos@humanoriented.com>
2022-01-21 22:42:16 -0800

    Test config: Use Swap rack_test for selenium drivers.
    Selenium/browser-driven system tests are great but they are slow.
    They are only necessary when there is js in the client. We do not
    have js running in the client right now.
    
    Speed up the tests by using rack_test driver instead of selenium.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,13 +1,14 @@
 ENV['RAILS_ENV'] ||= 'test'
-require_relative "../config/environment"
-require "rails/test_help"
+require_relative '../config/environment'
+require 'rails/test_help'
 
 class ActiveSupport::TestCase
-  # Run tests in parallel with specified workers
-  parallelize(workers: :number_of_processors)
 
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+  parallelize(workers: :number_of_processors)
   fixtures :all
 
-  # Add more helper methods to be used by all tests here...
+  def defines_before_filter?(controller_class, method_symbol)
+    controller_class._process_action_callbacks.map {|c| c.filter if c.kind == :before}.compact.include? method_symbol
+  end
+
 end
