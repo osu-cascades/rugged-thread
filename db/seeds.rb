6c6afcda54741d81d338cd207ba4fda61d1b4212
Yong Bakos <ybakos@humanoriented.com>
2022-01-22 10:04:15 -0800

    DB: Add sample users to seed file.
    References #20, #27.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!(name: 'Developer Admin', email: 'admin@example.com',
+ password: 'password', password_confirmation: 'password', role: 'admin')
+User.create!(name: 'Developer Staff', email: 'staff@example.com',
+ password: 'password', password_confirmation: 'password', role: 'staff')
+User.create!(name: 'Developer Deactivated', email: 'deactivated@example.com',
+ password: 'password', password_confirmation: 'password', role: 'admin',
+ status: 'inactive')
