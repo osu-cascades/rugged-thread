c20a124acf2c9455d7ce2ad171b8a16433f04c99
Brayden Brown <braydenbrown@gmail.com>
2022-01-21 16:23:29 -0800

    Write unit tests for item statuses #23

--- test/models/item_status_test.rb
+++ test/models/item_status_test.rb
@@ -1,7 +1,23 @@
 require "test_helper"
 
 class ItemStatusTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'Item Status has a name' do
+    assert_respond_to(ItemStatus.new, :name)
+  end
+
+
+  test 'Item Status without a name is invalid' do
+    item_status = item_statuses(:one)
+    assert item_status.valid?
+    item_status.name = nil
+    refute item_status.valid?
+  end
+
+  test 'Item Status with a non-unique name is invalid' do
+    existing_item_status_name = item_statuses(:one).name
+    item_status = item_statuses(:two)
+    assert item_status.valid?
+    item_status.name = existing_item_status_name
+    refute item_status.valid?
+  end
 end
