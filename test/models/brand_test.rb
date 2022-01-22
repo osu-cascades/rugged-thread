ee77c036b6d13014b69be9eaa27713a5f5ad105f
Brayden Brown <braydenbrown@gmail.com>
2022-01-21 16:17:23 -0800

    Write unit tests for brands and modify discount fixture #18

--- test/models/brand_test.rb
+++ test/models/brand_test.rb
@@ -1,7 +1,23 @@
 require "test_helper"
 
 class BrandTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'Brand has a name' do
+    assert_respond_to(Brand.new, :name)
+  end
+
+
+  test 'Brand without a name is invalid' do
+    brand = brands(:one)
+    assert brand.valid?
+    brand.name = nil
+    refute brand.valid?
+  end
+
+  test 'Brand with a non-unique name is invalid' do
+    existing_brand_name = brands(:one).name
+    brand = brands(:two)
+    assert brand.valid?
+    brand.name = existing_brand_name
+    refute brand.valid?
+  end
 end
