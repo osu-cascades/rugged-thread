af6801420cd072acf8de8e6422cf942c8c8f8296
Brayden Brown <braydenbrown@gmail.com>
2022-01-21 16:26:24 -0800

    Write unit tests for fee #24

--- test/models/fee_test.rb
+++ test/models/fee_test.rb
@@ -1,7 +1,30 @@
 require "test_helper"
 
 class FeeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'Fee has a description' do
+    assert_respond_to(Fee.new, :description)
+  end
+
+
+  test 'Fee without a description is invalid' do
+    fee = fees(:one)
+    assert fee.valid?
+    fee.description = nil
+    refute fee.valid?
+  end
+
+  test 'Fee without a non-integer value is invalid' do
+    fee = fees(:one)
+    assert fee.valid?
+    fee.price = "FakePrice"
+    refute fee.valid?
+  end
+
+  test 'Fee with a non-unique description is invalid' do
+    existing_fee_description = fees(:one).description
+    fee = fees(:two)
+    assert fee.valid?
+    fee.description = existing_fee_description
+    refute fee.valid?
+  end
 end
