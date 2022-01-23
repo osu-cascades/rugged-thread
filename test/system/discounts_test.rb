a73dddea9cc9a219d71ec9512d024515815594c3
Brayden Brown <braydenbrown@gmail.com>
2022-01-23 11:25:47 -0800

    Valdiations and system testing for discounts may need clarification with fields and value decisions #25

--- test/system/discounts_test.rb
+++ test/system/discounts_test.rb
@@ -1,8 +1,12 @@
 require "application_system_test_case"
 
 class DiscountsTest < ApplicationSystemTestCase
+
+  include Devise::Test::IntegrationHelpers
+
   setup do
     @discount = discounts(:one)
+    sign_in users(:staff)
   end
 
   test "visiting the index" do
@@ -14,8 +18,9 @@ class DiscountsTest < ApplicationSystemTestCase
     visit discounts_path
     click_on "New Discount"
 
-    fill_in "Amount", with: @discount.amount
-    fill_in "Description", with: @discount.description
+    fill_in "Description", with: "Fake Item Description"
+    fill_in "Percentage amount", with: @discount.percentage_amount
+    fill_in "Dollar amount", with: @discount.dollar_amount
     click_on "Create Discount"
 
     assert_text "Discount was successfully created"
@@ -26,11 +31,97 @@ class DiscountsTest < ApplicationSystemTestCase
     visit discounts_path
     click_on "Edit", match: :first
 
-    fill_in "Amount", with: @discount.amount
     fill_in "Description", with: @discount.description
+    fill_in "Percentage amount", with: @discount.percentage_amount
+    fill_in "Dollar amount", with: @discount.dollar_amount
     click_on "Update Discount"
 
     assert_text "Discount was successfully updated"
+    click_on "Back"
+  end
+
+  test "creating a blank Discount description" do
+    visit discounts_path
+    click_on "New Discount"
+
+    fill_in "Description", with: ""
+    fill_in "Percentage amount", with: @discount.percentage_amount
+    fill_in "Dollar amount", with: @discount.dollar_amount
+
+    click_on "Create Discount"
+
+    assert_text "Description can't be blank"
+    click_on "Back"
+  end
+
+  test "creating a duplicate Discount description" do
+    visit discounts_path
+    click_on "New Discount"
+
+    fill_in "Description", with: @discount.description
+    fill_in "Percentage amount", with: @discount.percentage_amount
+    fill_in "Dollar amount", with: @discount.dollar_amount
+
+    click_on "Create Discount"
+
+    assert_text "Description has already been taken"
+    click_on "Back"
+  end
+
+
+  test "creating a non-integer Percentage Amount price" do
+    visit discounts_path
+    click_on "New Discount"
+
+    fill_in "Description", with: "Fake Description for Percentage non-integer test"
+    fill_in "Percentage amount", with: "Fake Percentage"
+    fill_in "Dollar amount", with: @discount.dollar_amount
+
+    click_on "Create Discount"
+
+    assert_text "Percentage amount is not a number"
+    click_on "Back"
+  end
+
+  test "creating a non-integer Dollar Amount price" do
+    visit discounts_path
+    click_on "New Discount"
+
+    fill_in "Description", with: "Fake Description for  Dollar non-integer test"
+    fill_in "Percentage amount", with: @discount.percentage_amount
+    fill_in "Dollar amount", with: "Fake Dollar"
+
+    click_on "Create Discount"
+
+    assert_text "Dollar amount is not a number"
+    click_on "Back"
+  end
+
+  test "creating a float Percentage Amount price" do
+    visit discounts_path
+    click_on "New Discount"
+
+    fill_in "Description", with: "Fake Description for Percentage float test"
+    fill_in "Percentage amount", with: 20.5
+    fill_in "Dollar amount", with: @discount.dollar_amount
+
+    click_on "Create Discount"
+
+    assert_text "Percentage amount must be an integer"
+    click_on "Back"
+  end
+
+  test "creating a float Dollar Amount price" do
+    visit discounts_path
+    click_on "New Discount"
+
+    fill_in "Description", with: "Fake Description for  Dollar float test"
+    fill_in "Percentage amount", with: @discount.percentage_amount
+    fill_in "Dollar amount", with: 19.8
+
+    click_on "Create Discount"
+
+    assert_text "Dollar amount must be an integer"
     click_on "Back"
   end
 
