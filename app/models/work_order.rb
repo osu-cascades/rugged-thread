d9cbfb007667d2de6d30b89044f18d4cc3b51572
Yong Bakos <ybakos@humanoriented.com>
2022-01-26 00:00:00 -0800

    WorkOrder: Test deletion restriction for item association.
    Fix the items fixture, to associate items with a
    correctly-named work order.

--- app/models/work_order.rb
+++ app/models/work_order.rb
@@ -1,4 +1,4 @@
 class WorkOrder < ApplicationRecord
   belongs_to :customer
-  has_many :items
+  has_many :items, dependent: :restrict_with_error
 end
--- test/models/work_order_test.rb
+++ test/models/work_order_test.rb
@@ -2,8 +2,19 @@ require "test_helper"
 
 class WorkOrderTest < ActiveSupport::TestCase
 
+  test "belongs to a customer" do
+    assert_respond_to(WorkOrder.new, :customer)
+  end
+
   test "has many items" do
     assert_respond_to(WorkOrder.new, :items)
+  end
+
+  test "cannot be deleted if it has associated items" do
+    work_order = work_orders(:shipping)
+    assert_not_empty work_order.items
+    work_order.destroy
+    refute work_order.destroyed?
   end
 
 end
