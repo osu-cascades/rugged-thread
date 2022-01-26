d0c96d0f8b9e228ca7f054817ed1758bed2613f1
Yong Bakos <ybakos@humanoriented.com>
2022-01-25 20:26:34 -0800

    Seeds: Add WorkOrder, Brands, ItemStatuses, and ItemTypes.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,18 @@ User.create!(name: 'Developer Deactivated', email: 'deactivated@example.com',
  password: 'password', password_confirmation: 'password', role: 'admin',
  status: 'inactive')
  
-Customer.create!(  first_name: 'Example', last_name: 'Customer',
+customer = Customer.create!(  first_name: 'Example', last_name: 'Customer',
  business_name: 'Example Business', phone_number: '541-555-5555',
  email_address: 'exampleCustomer@email.com', street_address: '123 Sesame Street', city: 'Bend', 
  state: 'OR', zip_code: '97704')
+
+WorkOrder.create!(in_date: Date.today, estimate: 100, shipping: true, customer: customer)
+
+Brand.create!(name: 'Example Brand One')
+Brand.create!(name: 'Example Brand Two')
+
+ItemStatus.create!(name: 'Example Status One')
+ItemStatus.create!(name: 'Example Status Two')
+
+ItemType.create(name: 'Example Item Type One')
+ItemType.create(name: 'Example Item Type Two')
