fc8ee1c5391fddd2e0d61cec48d665c1c8754a1e
Yong Bakos <ybakos@humanoriented.com>
2022-01-27 00:47:22 -0800

    ApplicationController: Remove restrict_unless_admin.
    Only UsersController was using this custom action, and
    UsersController now leverages pundit and the UserPolicy.
    
    Future admin-only features will leverage Pundit and
    policy classes.
    
    References #38.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,6 @@ class ApplicationController < ActionController::Base
 
   private
     
-    def restrict_unless_admin
-      redirect_to(root_url, alert: 'Access denied.') unless current_user.admin?
-    end
-
     def after_sign_out_path_for(resource)
       new_user_session_path
     end
