0942d5b2cf2ba079351d868e437fd7d0a43096d1
Yong Bakos <ybakos@humanoriented.com>
2022-01-27 22:25:04 -0800

    Work Order Item: Improve show view layout and styles.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,13 +1,14 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: %i[ show edit update destroy ]
+  before_action :set_item, only: %i[ edit update destroy ]
 
   # GET /items or /items.json
   def index
     @items = Item.all
   end
 
-  # GET /items/1 or /items/1.json
   def show
+    @work_order = WorkOrder.find(params[:work_order_id])
+    @item = @work_order.items.find(params[:id])
   end
 
   # GET /items/new
