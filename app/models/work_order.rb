b4b0ef3d22d0dac31d5bdc0acb03ed2d570545b2
Yong Bakos <ybakos@humanoriented.com>
2022-01-30 10:25:31 -0800

    WorkOrder: Refactor tests. Add in_date validator.

--- app/models/work_order.rb
+++ app/models/work_order.rb
@@ -3,6 +3,8 @@ class WorkOrder < ApplicationRecord
   belongs_to :customer
   has_many :items, dependent: :restrict_with_error
 
+  validates :in_date, presence: true
+
   default_scope { order('created_at ASC') }
 
   def to_s
--- test/models/work_order_test.rb
+++ test/models/work_order_test.rb
@@ -2,11 +2,13 @@ require "test_helper"
 
 class WorkOrderTest < ActiveSupport::TestCase
 
-  test "belongs to a customer" do
-    assert_respond_to(WorkOrder.new, :customer)
+  test 'attributes'do
+    assert_respond_to(WorkOrder.new, :in_date)
+    assert_respond_to(WorkOrder.new, :shipping)
   end
 
-  test "has many items" do
+  test 'relationships' do
+    assert_respond_to(WorkOrder.new, :customer)
     assert_respond_to(WorkOrder.new, :items)
   end
 
@@ -21,6 +23,15 @@ class WorkOrderTest < ActiveSupport::TestCase
     work_order = work_orders(:shipping)
     expected = "Work Order #{work_order.id}"
     assert_equal expected, work_order.to_s
+  end
+
+  test 'is invalid without a date' do
+    work_order = work_orders(:shipping)
+    assert work_order.valid?
+    work_order.in_date = nil
+    refute work_order.valid?
+    work_order.in_date = ''
+    refute work_order.valid?
   end
 
 end
