37133942a02d2732aeee23a25b04036a5605b4f4
Yong Bakos <ybakos@humanoriented.com>
2022-01-31 21:42:57 -0800

    ItemStatus: Combine validator.
    Name presence and uniqueness can be one declaration.

--- app/models/item_status.rb
+++ app/models/item_status.rb
@@ -2,8 +2,7 @@ class ItemStatus < ApplicationRecord
 
   has_many :items, dependent: :restrict_with_error
 
-  validates :name, presence: true
-  validates :name, uniqueness: true
+  validates :name, presence: true, uniqueness: true
 
   default_scope { order('name ASC') }
 
