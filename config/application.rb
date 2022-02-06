3d88e5afa9ed92c720816be0ecbc2bee02a12adf
Yong Bakos <ybakos@humanoriented.com>
2022-02-06 02:26:25 -0800

    Config: Update main configuration.
    Rename the application to RuggedThread.
    Add encrypted credentials files.
    Remove root default credentials file.
    Add production config.
    Add airbrake initializer.
    
    References #92.

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module RuggedThreadUpgrade
+module RuggedThread
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
@@ -16,7 +16,7 @@ module RuggedThreadUpgrade
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = 'Pacific Time (US & Canada)'
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
