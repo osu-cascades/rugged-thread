3f6f7c00bb82eafb5670c1c5e286dcd7397b58d7
Yong Bakos <ybakos@humanoriented.com>
2022-02-07 23:38:14 -0800

    Repair: Test attribtues.
    References #87, #88.

--- test/models/repair_test.rb
+++ test/models/repair_test.rb
@@ -2,6 +2,11 @@ require "test_helper"
 
 class RepairTest < ActiveSupport::TestCase
 
+  test "attributes" do
+    assert_respond_to(Repair.new, :notes)
+    assert_respond_to(Repair.new, :price)
+  end
+
   test "associations" do
     assert_respond_to(Repair.new, :item)
     assert_respond_to(Repair.new, :standard_repair)
