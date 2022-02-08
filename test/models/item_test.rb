e181f2a4f0a03a8b63a5b97497eb9197d02a8080
Yong Bakos <ybakos@humanoriented.com>
2022-02-07 23:48:42 -0800

    Item: Add characterization tests, test default status.
    References #54, #55.

--- test/models/item_test.rb
+++ test/models/item_test.rb
@@ -2,20 +2,24 @@ require "test_helper"
 
 class ItemTest < ActiveSupport::TestCase
 
-  test "belongs to a brand" do
+  test "attributes" do
+    assert_respond_to(Item.new, :due_date)
+    assert_respond_to(Item.new, :estimate)
+    assert_respond_to(Item.new, :labor_estimate)
+    assert_respond_to(Item.new, :notes)
+  end
+
+  test "associations" do
     assert_respond_to(Item.new, :brand)
-  end
-
-  test "belongs to a work order" do
-    assert_respond_to(Item.new, :work_order)
-  end
-
-  test "belongs to an item type" do
-    assert_respond_to(Item.new, :item_type)
-  end
-
-  test "belongs to an item status" do
     assert_respond_to(Item.new, :item_status)
+    assert_respond_to(Item.new, :item_type)
+    assert_respond_to(Item.new, :work_order)
+    assert_respond_to(Item.new, :repairs)
+  end
+
+  test "has a status that is the default item status" do
+    item = Item.new
+    assert_equal(ItemStatus.default, item.item_status)
   end
 
 end
