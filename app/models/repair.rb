9611a59c6be0535dbdd100436aba24f6d3dc761e
Yong Bakos <ybakos@humanoriented.com>
2022-02-13 11:13:00 -0800

    Repair: Add validations and test.
    References #104.

--- app/models/repair.rb
+++ app/models/repair.rb
@@ -1,4 +1,8 @@
 class Repair < ApplicationRecord
+
   belongs_to :standard_repair
   belongs_to :item
+
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
 end
--- test/models/repair_test.rb
+++ test/models/repair_test.rb
@@ -12,9 +12,31 @@ class RepairTest < ActiveSupport::TestCase
     assert_respond_to(Repair.new, :standard_repair)
   end
 
+  test "must have a standard repair" do
+    repair = repairs(:one)
+    assert repair.valid?
+    repair.standard_repair = nil
+    refute repair.valid?
+  end
+
+  test "must have an item" do
+    repair = repairs(:one)
+    assert repair.valid?
+    repair.item = nil
+    refute repair.valid?
+  end
+
   test "has a price that is the default repair price" do
     repair = Repair.new
     assert_equal(repair.price, 0)
   end
+
+  test 'price must be a positive integer' do
+    repair = repairs(:one)
+    assert repair.valid?
+    repair.price = -1
+    refute repair.valid?
+  end
+
 
 end
