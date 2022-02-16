36b9b0ba19e00b2a0cd0403b24fb241eb60360e1
Brayden Brown <braydenbrown@gmail.com>
2022-02-16 14:41:39 -0800

    Change validation to allow nil for percentage and dollar amount #118

--- app/models/standard_discount.rb
+++ app/models/standard_discount.rb
@@ -3,8 +3,8 @@ class StandardDiscount < ApplicationRecord
   has_many :discounts, dependent: :restrict_with_error
   
   validates :name, presence: true, uniqueness: true
-  validates :percentage_amount, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :dollar_amount, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :percentage_amount, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, allow_nil: true
+  validates :dollar_amount, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, allow_nil: true
 
   default_scope { order('name ASC') }
 
