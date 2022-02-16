d4021201fa7f4fb67db576f2f14a772c5f190ee1
Brayden Brown <braydenbrown@gmail.com>
2022-02-15 22:57:09 -0800

    Add associations test #110, add test for deletion of model #111, test numericality of dollar and percentage amount for standard discount #112

--- app/models/standard_discount.rb
+++ app/models/standard_discount.rb
@@ -3,8 +3,8 @@ class StandardDiscount < ApplicationRecord
   has_many :discounts, dependent: :restrict_with_error
   
   validates :name, presence: true, uniqueness: true
-  validates :percentage_amount , numericality: { only_integer: true }, allow_nil: true
-  validates :dollar_amount , numericality: { only_integer: true }, allow_nil: true
+  validates :percentage_amount, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :dollar_amount, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   default_scope { order('name ASC') }
 
--- test/models/standard_discount_test.rb
+++ test/models/standard_discount_test.rb
@@ -8,10 +8,35 @@ class StandardDiscountTest < ActiveSupport::TestCase
     assert_respond_to(StandardDiscount.new, :dollar_amount)
   end
 
+  test "associations" do
+    assert_respond_to(StandardDiscount.new, :discounts)
+  end
+
+  test 'cannot be deleted if it has associated discounts' do
+    standard_discount = standard_discounts(:one)
+    assert_not_empty standard_discount.discounts
+    standard_discount.destroy
+    refute standard_discount.destroyed?
+  end
+
   test 'must have a name' do
     standard_discount = standard_discounts(:one)
     assert standard_discount.valid?
     standard_discount.name = nil
+    refute standard_discount.valid?
+  end
+
+  test 'percentage_amount must be a positive integer' do
+    standard_discount = standard_discounts(:one)
+    assert standard_discount.valid?
+    standard_discount.percentage_amount = -1
+    refute standard_discount.valid?
+  end
+
+  test 'dollar_amount must be a positive integer' do
+    standard_discount = standard_discounts(:one)
+    assert standard_discount.valid?
+    standard_discount.dollar_amount = -1
     refute standard_discount.valid?
   end
 
