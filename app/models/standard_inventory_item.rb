85d8ac37a536cefc36bbb1942f1e50ece3c75784
Brayden Brown <braydenbrown@gmail.com>
2022-02-28 11:56:02 -0800

    Change model validations to be correct for standard inventory item #153

--- app/models/standard_inventory_item.rb
+++ app/models/standard_inventory_item.rb
@@ -1,6 +1,6 @@
 class StandardInventoryItem < ApplicationRecord
-  validates :name, presence: true, unique: true
-  validates :sku, presence: true, unique: true
+  validates :name, presence: true, uniqueness: true
+  validates :sku, presence: true, uniqueness: true
   validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   default_scope { order('name ASC') }
