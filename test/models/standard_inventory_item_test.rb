ce7dc16df585dd7db7287fa9fe4e270cebca231a
Yong Bakos <ybakos@humanoriented.com>
2022-02-28 01:17:20 -0800

    StandardInventoryItem: Stub model tests.
    References #153.

--- test/models/standard_inventory_item_test.rb
+++ test/models/standard_inventory_item_test.rb
@@ -1,7 +1,45 @@
 require "test_helper"
 
 class StandardInventoryItemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "attributes" do
+    skip
+  end
+
+  test "associations" do
+    skip
+  end
+
+  test "cannot be deleted if it has associated inventory items" do
+    skip
+  end
+
+  test "is not valid without a name" do
+    skip
+  end
+
+  test "name must be unique" do
+    skip
+  end
+
+  test "is not valid without a sku" do
+    skip
+  end
+
+  test "sku must be unique" do
+    skip
+  end
+
+  test "is not valid without a name" do
+    skip
+  end
+
+  test "price must be a non-negative integer" do
+    skip
+  end
+
+  test "#to_s string representation is name" do
+    skip
+  end
+
 end
