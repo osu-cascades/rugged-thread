2ce4d1cc67edc143ff2f3ea91005faba8940afa2
Brayden Brown <braydenbrown@gmail.com>
2022-02-28 11:50:15 -0800

    Model validations for name price and sku #153

--- app/models/standard_inventory_item.rb
+++ app/models/standard_inventory_item.rb
@@ -1,3 +1,11 @@
 class StandardInventoryItem < ApplicationRecord
-  belongs_to :standard_repair
+  validates :name, presence: true, unique: true
+  validates :sku, presence: true, unique: true
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  default_scope { order('name ASC') }
+
+  def to_s
+    name
+  end
 end
