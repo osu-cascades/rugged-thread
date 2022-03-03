904d9c88051a200c3cae2e7c854548521f18ebb6
andras <andrasm96@gmail.com>
2022-03-02 16:45:41 -0800

    Repair Price JS Update
    Recieve promise via fetch
    for repair price
    but not quite there.
    
    References #154

--- app/javascript/custom/auto_population.js
+++ app/javascript/custom/auto_population.js
@@ -1,4 +1,6 @@
-function autofill() {
-    var standard_repair = '<%= @standard_repair.to_json %>';
-    document.getElementById('#repair_price') = standard_repair.price; 
-  };
+// function autofill() {
+//   var standard_repair = '<%= @standard_repair.to_json %>';
+//   document.getElementById('#repair_price') = standard_repair.price; 
+// };
+// 
+// console.log('Hello!');
