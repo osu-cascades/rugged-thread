7ab339ac366391214bdbcd51f8e949c1e21ef85a
Brayden Brown <braydenbrown@gmail.com>
2022-03-07 19:39:28 -0800

    Customer: Add validation using phony nomalization aand validation #165

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,7 +2,8 @@ class Customer < ApplicationRecord
 
   belongs_to :customer_type, counter_cache: true
   has_many :work_orders, dependent: :restrict_with_error
-
+  phony_normalize :phone_number, default_country_code: 'US'
+  validates :phone_number, phony_plausible: true
   default_scope { order('last_name ASC') }
 
   def full_name
