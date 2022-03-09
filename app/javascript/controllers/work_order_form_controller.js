811fca8c08be717ec74c4b097956753b6fd31f85
Yong Bakos <ybakos@humanoriented.com>
2022-03-09 10:24:58 -0800

    WorkOrder: Replace stimulus and fetch with turbo frame.
    Wrap the form in a turbo frame.
    Define a new implementation of setDueDate that just updates the frame.
    Delete everything that is now unnecessary.
    
    References #170.

--- app/javascript/controllers/work_order_form_controller.js
+++ app/javascript/controllers/work_order_form_controller.js
@@ -1,31 +1,12 @@
 import { Controller } from "@hotwired/stimulus"
-import { addDays, formatISO, parseISO } from "date-fns"
 
 export default class extends Controller {
 
-  async setDueDate() {
-    if (this.blankElementSelected()) return
-    const dueDateField = document.getElementById('work_order_due_date')
-    this.prepareForAnimation(dueDateField)
-    const customerId = this.element.value
-    const customerTypeId = await fetch(`/customers/${customerId}.json`)
-                                   .then(response => response.json())
-                                   .then(data => data.customer_type_id)
-    fetch(`/customer_types/${customerTypeId}.json`)
-      .then(response => response.json())
-      .then(data => {
-        dueDateField.value = this.format(
-          this.dueDate(this.inDate(), data.turn_around)
-        )
-        this.highlight(dueDateField)
-      })
+  setDueDate() {
+    if (this.blankOptionSelected()) return
+    Turbo.visit(`/customers/${this.element.value}/work_orders/new`)
   }
 
-  blankElementSelected = () => this.element.value === ''
-  prepareForAnimation = (field) => field.classList.remove('highlight')
-  inDate = () => document.getElementById('work_order_in_date').value
-  dueDate = (start, turnAround) => addDays(parseISO(start), turnAround)
-  format = (dueDate) => formatISO(dueDate, {representation: 'date'})
-  highlight = (field) => field.classList.add('highlight')
+  blankOptionSelected = () => this.element.value === ''
 
 }
--- config/importmap.rb
+++ config/importmap.rb
@@ -6,4 +6,3 @@ pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin_all_from "app/javascript/custom", under: "custom"
-pin "date-fns", to: "https://ga.jspm.io/npm:date-fns@2.28.0/esm/index.js"
