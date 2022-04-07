a3caa90cf734c25a22d89f46cfa910ba61962827
Yong Bakos <ybakos@humanoriented.com>
2022-04-06 21:17:45 -0700

    WorkOrderNumberTest: Rename file and use regex for test.
    The test fails because it had a hardcoded month and time has passed.
    Use a regex instead.
    
    Correct the filename - this is a WorkOrderNumberTest, not a WorkOrderTest.

--- test/lib/work_order_number_test.rb
+++ test/lib/work_order_number_test.rb
@@ -4,7 +4,7 @@ require 'work_order_number'
 class WorkOrderNumberTest < ActiveSupport::TestCase
 
   test '#number_for_month returns the number of WorkOrders created during that month' do
-    assert_equal "2203-000#{work_orders.count + 1}", WorkOrderNumber.to_s
+    assert_match /\d\d\d\d-\d\d\d#{work_orders.count + 1}/, WorkOrderNumber.to_s
   end
 
 end
