362a5ab4990ca77d11ed8d2dc59ba5427fc1e096
Yong Bakos <ybakos@humanoriented.com>
2022-04-14 16:43:39 -0700

    StandardRepair: Expand name uniqueness to scope of method.
    Currently, names must be unique. The client would like to use the same name
    for standard repairs that have different methods.
    
    Add a 'method' scope for the name uniqueness validation.
    
    References #197.

--- app/models/standard_repair.rb
+++ app/models/standard_repair.rb
@@ -3,7 +3,7 @@ class StandardRepair < ApplicationRecord
   has_many :repairs, dependent: :restrict_with_error
   has_many :standard_complications, dependent: :restrict_with_error
 
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: { scope: :method }
   validates :level, numericality: { only_integer: true, greater_than: 0 }
   validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
