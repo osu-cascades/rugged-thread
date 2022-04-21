ee7affc2d77e7184becc2932c723556c42531cbb
Yong Bakos <ybakos@humanoriented.com>
2022-04-21 14:58:42 -0700

    Fee: Migrate price to Money object.
    Rename price column to price_cents.
    Monetize price_cents.
    Update fixture and unit test.
    
    References #159.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_21_213844) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_21_215443) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -125,7 +125,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_21_213844) do
     t.bigint "item_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "price", default: 0, null: false
+    t.integer "price_cents", default: 0, null: false
     t.integer "position"
     t.index ["item_id"], name: "index_fees_on_item_id"
     t.index ["standard_fee_id"], name: "index_fees_on_standard_fee_id"
