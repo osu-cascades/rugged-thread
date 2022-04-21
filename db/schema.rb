6d09464909653be6e4166d483dc15893d5e6683d
Yong Bakos <ybakos@humanoriented.com>
2022-04-21 15:43:02 -0700

    StandardFee: Migrate price to Money object.
    Rename price column to price_cents.
    Monetize price_cents.
    Update fixture and unit test.
    
    References #159.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_21_223009) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_21_223803) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -272,7 +272,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_21_223009) do
 
   create_table "standard_fees", force: :cascade do |t|
     t.text "name"
-    t.integer "price", default: 0, null: false
+    t.integer "price_cents", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
