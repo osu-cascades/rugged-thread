366e42cc226a5ee00c1c479970ca708ab9f698ac
Yong Bakos <ybakos@humanoriented.com>
2022-04-21 15:02:29 -0700

    InventoryItem: Migrate price to Money object.
    Rename price column to price_cents.
    Monetize price_cents.
    Update fixture and unit test.
    
    References #159.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_21_215443) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_21_215921) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -132,7 +132,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_21_215443) do
   end
 
   create_table "inventory_items", force: :cascade do |t|
-    t.integer "price", default: 0, null: false
+    t.integer "price_cents", default: 0, null: false
     t.bigint "repair_id", null: false
     t.bigint "standard_inventory_item_id", null: false
     t.datetime "created_at", null: false
