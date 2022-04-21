ce7997571495bc91fac295d23325aae2a618d407
Yong Bakos <ybakos@humanoriented.com>
2022-04-21 15:29:43 -0700

    StandardComplication: Migrate price to Money object.
    Rename price column to price_cents.
    Monetize price_cents.
    Update fixture and unit test.
    
    References #159.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_21_220711) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_21_222714) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -255,7 +255,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_21_220711) do
 
   create_table "standard_complications", force: :cascade do |t|
     t.string "name"
-    t.integer "price", default: 0, null: false
+    t.integer "price_cents", default: 0, null: false
     t.bigint "standard_repair_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
