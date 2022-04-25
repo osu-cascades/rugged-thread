4cd091555420d325564e14108fbe75c22b20caec
dandyandy22 <andrasm96@gmail.com>
2022-04-25 11:57:43 -0700

    Application Helper: Generalize archive button helper
    Refactor and adapt helper method to
    accept other classes.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,11 +5,8 @@ module ApplicationHelper
     render 'shared/pagination_nav', { pagy: pagy, prev_path: prev_path, next_path: next_path }
   end
 
-  def archived?(object)
-    if object.kept?
-      return button_to 'Archive', archive_customer_path(object), method: :patch, class: 'btn btn-outline-success me-2'
-    else
-      return button_to 'Recover', recover_customer_path(object), method: :patch, class: 'btn btn-outline-success me-2'
-    end
+  def link_to_archive_or_recover(object)
+    action_name = object.kept? ? 'Archive' : 'Recover'
+    button_to action_name, send("#{action_name.downcase}_#{object.class.name.underscore}_path", object), method: :patch, class: 'btn btn-outline-success me-2'
   end
 end
