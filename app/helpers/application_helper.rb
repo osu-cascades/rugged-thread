16625c8cdf486e2298e9a6fb6251bfc6bdddda4f
Yong Bakos <ybakos@humanoriented.com>
2022-04-26 13:54:20 -0700

    Customer: Wrap address render in helper.
    Shipping and billing addresses appear as a comma when the address,
    especially city / state / zip are blank. Wrap the presentation logic
    in a helper method, to ensure that a blank address never appears as
    a dangling comma in the view.
    
    References #220.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,14 @@ module ApplicationHelper
     action_name = object.kept? ? 'Archive' : 'Recover'
     button_to action_name, send("#{action_name.downcase}_#{object.class.name.underscore}_path", object), method: :patch, class: 'btn btn-outline-success me-2'
   end
+
+  def present_address_city_state_zip(city, state, zip)
+    if city.blank? && state.blank?
+      zip
+    elsif city.blank? && state.present?
+      "#{state} #{zip}"
+    elsif city.present? && state.blank?
+      "#{city} #{zip}"
+    end
+  end
 end
