9a51f9c2fc576629d9b412cd6671273caf1d4384
Yong Bakos <ybakos@humanoriented.com>
2022-05-03 00:12:18 -0700

    Dashboard: Calculate and display estimated work order price total and count.
    References #232.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,9 @@
 class DashboardController < ApplicationController
 
   def show
+    @open_work_orders = WorkOrder.open
+    @number_of_open_work_orders = @open_work_orders.size
+    @total_price_of_open_work_orders = @open_work_orders.reduce(Money.new(0)) { |sum, wo| sum + wo.price }
   end
 
 end
