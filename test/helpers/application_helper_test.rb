80ec7d8ec4e776f92d273228c9e59ffc6b85b99c
Yong Bakos <ybakos@humanoriented.com>
2022-05-10 07:35:32 -0700

    ApplicationHelper: Stub.
    References #245.

---
+++ test/helpers/application_helper_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class ApplicationHelperTest < ActionView::TestCase
+
+  # present_address_city_state_zip
+
+  test 'TODO' do
+    skip
+  end
+
+  test 'TODO 2' do
+  end
+
+end
