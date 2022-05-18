df607059014364f1b532f2bd5ceeb10375a5f43b
Yong Bakos <ybakos@humanoriented.com>
2022-05-17 22:27:10 -0700

    Dashboard: Display ty total sum price of non-invoiced items.
    References #249.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,9 +1,9 @@
 class DashboardController < ApplicationController
 
   def show
-    @open_work_orders = WorkOrder.open
-    @number_of_open_work_orders = @open_work_orders.size
-    @total_price_of_open_work_orders = @open_work_orders.reduce(Money.new(0)) { |sum, wo| sum + wo.price }
+    non_invoiced_items = Item.kept.not_invoiced
+    @number_of_non_invoiced_items = non_invoiced_items.size
+    @total_price_of_non_invoiced_items = non_invoiced_items.reduce(Money.new(0)) { |sum, i| sum + i.price }
   end
 
 end
--- test/system/dashboard_test.rb
+++ test/system/dashboard_test.rb
@@ -13,4 +13,12 @@ class DashboardTest < ApplicationSystemTestCase
     assert_selector "h1", text: "Dashboard"
   end
 
+  test "open order total price is the sum of all kept non-invoiced items" do
+    total_price_of_non_invoiced_items = Item.kept.not_invoiced.reduce(0) { |sum, i| sum + i.price }
+    number_of_non_invoiced_items = Item.kept.not_invoiced.count
+    visit dashboard_path
+    assert_text total_price_of_non_invoiced_items.format
+    assert_text "#{number_of_non_invoiced_items} items not yet invoiced"
+  end
+
 end
