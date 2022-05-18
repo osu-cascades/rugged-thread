a16f4f0163263948c0ecedd633928b70ebe9ab0a
Yong Bakos <ybakos@humanoriented.com>
2022-05-18 02:01:21 -0700

    Customer: Adjust whitespace.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -5,10 +5,12 @@ class Customer < ApplicationRecord
 
   belongs_to :customer_type, counter_cache: true
   has_many :work_orders, dependent: :restrict_with_error
+
   phony_normalize :phone_number, default_country_code: 'US'
   validates :phone_number, phony_plausible: true
   phony_normalize :alternative_phone_number, default_country_code: 'US'
   validates :alternative_phone_number, phony_plausible: true
+
   default_scope { order('last_name ASC') }
 
   def full_name
