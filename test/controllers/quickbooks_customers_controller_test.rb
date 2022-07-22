2e7c313687f1c9450674d24d8fc24a632c663e71
Yong Bakos <ybakos@humanoriented.com>
2022-07-22 10:43:16 -0700

    Controller tests: Remove qbo controller tests.
    Modern rails doesn't emphasize controller tests, since they
    tend to duplicate coverage provided by system tests. We do / will
    want some controller tests eventually, to verify secure access and
    redirects. But for now, remove these controller tests.

--- test/controllers/quickbooks_customers_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class QuickbooksCustomersControllerTest < ActionDispatch::IntegrationTest
-  test "should get show" do
-    get quickbooks_customers_show_url
-    assert_response :success
-  end
-end
