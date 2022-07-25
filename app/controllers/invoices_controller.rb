567fb82d137e94e8a9a4492e118e450144d110f2
theusaf <theusafyt@gmail.com>
2022-07-25 15:35:11 -0700

    Invoice: Create 'hello world' invoice generation
    * Adds a button that opens a tab in QuickBooks after creating the invoice
    * Very simple invoice creation
    * Resolves #293

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -1,11 +1,37 @@
-class InvoicesController < ApplicationController
+class InvoicesController < QuickbooksAbstractController
 
   def show
     @work_order = WorkOrder.find(params[:id])
   end
 
   def submit
-    @work_order = WorkOrder.find(params[:id])
+    work_order = WorkOrder.find(params[:id])
+    item = work_order.items.first
+    now = DateTime.now
+    payload = {
+      "CustomerRef" => {
+        # TODO: Replace with dynamic/correct id when customer/quickbooks integration is more flushed out
+        "value" => "1"
+      },
+      # Basic invoice with 1 item
+      "Line" => [{
+        "DetailType" => "SalesItemLineDetail",
+        "Description" => item.item_type.name,
+        "Amount" => item.price.to_i,
+        "SalesItemLineDetail" => {
+          "ServiceDate" => "#{now.year}-#{now.month.to_s.rjust(2, "0")}-#{now.day.to_s.rjust(2, "0")}",
+          "ItemRef" => {
+            # TODO: Use Quickbooks to store Items, use that id here.
+            "value" => 1
+          }
+        }
+      }]
+    }
+    qb_request do
+      logger.info "\n\n\n#{payload.to_s}\n\n\n"
+      response = qb_api.create(:invoice, payload: payload)
+      redirect_to "https://app.qbo.intuit.com/app/invoice?txnId=#{response["Id"]}", allow_other_host: true
+    end
   end
 
 end
